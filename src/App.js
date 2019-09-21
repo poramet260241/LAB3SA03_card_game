16bf496cdb3cfa9360447768884f2f700b1a94c1
poramet260241 <oblivion2408@gmail.com>
2019-09-21 21:41:27 +0700

    check point 4-5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import WordCard from './WordCard'
 import './App.css';
+import CharacterCard from './CharacterCard';
+
+const word = "Hello";
+
 class App extends Component {
   render() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
- }
+    return (
+      <div>
+      {
+        <WordCard value="hello"/>
+      }
+      </div>
+    );
+  }
 }
-export default App;
 
-//aaaaa
+export default App 
